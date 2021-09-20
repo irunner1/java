082df5e307e27096e27b5a96327d8024270efece
irunner1 <aspuchkov02@gmail.com>
2021-09-21 00:02:08 +0300

    added task123

---
+++ task2/TestAuthor.java
@@ -0,0 +1,10 @@
+package task2;
+
+public class TestAuthor{
+    public static void main(String args[]) {
+        Author a1 = new Author("Michel", "asd@gmail.com", 'F');
+        a1.setEmail("das@gmail.com");
+        System.out.println(a1);
+    }
+
+}
---
+++ task3/testCircle.java
@@ -0,0 +1,15 @@
+package task3;
+
+public class testCircle {
+    public static void main(String[] args) {
+        Movable m1 = new movablePoint(2, 2, 10, 10);
+        System.out.println(m1);
+        m1.moveUp();
+        System.out.println(m1);
+           
+        Movable m2 = new MovableCircle(2, 1, 2, 2, 20);
+        System.out.println(m2);
+        m2.moveRight();
+        System.out.println(m2);
+    }
+}
